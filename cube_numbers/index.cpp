1645533ca10dfa30753b7e0633e61d1d57f6466c
HubiRed1 <hubert.redlinski88@gmail.com>
2026-03-18 21:11:18 +0100

    git commit

--- cube_numbers/index.cpp
+++ cube_numbers/index.cpp
@@ -1,82 +1,97 @@
 #include <iostream>
 #include <cstdlib> 
+#include <ctime> // Potrzebne do time()
 
 using namespace std;
 
+int rzutKostka() {   
+    return rand() % 6 + 1;
+}
 
-	int rzutKostka(){	
-	    int liczba = rand() % 6 + 1;
-	    return liczba;
-	}
-	
-	int dodajRzut(int rzuty[], int liczbaRzutow) {
-		int wynik = rzutKostka();
-		rzuty[liczbaRzutow] = wynik;
-		
-		cout<<wynik<<endl;
-		
-		return liczbaRzutow + 1;
-	}
-	
-	void wyswietlRzuty(int rzuty[], int liczbaRzutow) {
-		for(int i = 0; i < liczbaRzutow; i++){
-			cout<<rzuty[i]<<endl;
-		}
-	}
-	
-	void sumaOczek(int rzuty[], int liczbaRzutow) {
-		int suma = 0;
-		for(int i = 0; i < liczbaRzutow; i++){
-			suma += rzuty[i];
-		}
-		cout<<suma<<endl;
-	}
-	
-	void policzSzostki(int rzuty[], int liczbaRzutow) {
-		int szostki = 0;
-		for(int i = 0; i < liczbaRzutow; i++){
-			if(rzuty[]){
-				szostki++;
-			}
-		}
-	}
-	
-	void najwiekszyRzut(int rzuty[], int liczbaRzutow) {
-		
-	}
-	
- 
+int dodajRzut(int rzuty[], int liczbaRzutow) {
+    if (liczbaRzutow >= 50) {
+        cout << "Brak miejsca na kolejne rzuty!" << endl;
+        return liczbaRzutow;
+    }
+    int wynik = rzutKostka();
+    rzuty[liczbaRzutow] = wynik;
+    cout << "Wylosowano: " << wynik << endl;
+    return liczbaRzutow + 1;
+}
+
+void wyswietlRzuty(int rzuty[], int liczbaRzutow) {
+    cout << "Historia rzutow: ";
+    for(int i = 0; i < liczbaRzutow; i++) {
+        cout << rzuty[i] << " ";
+    }
+    cout << endl;
+}
+
+void sumaOczek(int rzuty[], int liczbaRzutow) {
+    int suma = 0;
+    for(int i = 0; i < liczbaRzutow; i++) {
+        suma += rzuty[i];
+    }
+    cout << "Suma oczek: " << suma << endl;
+}
+
+void policzSzostki(int rzuty[], int liczbaRzutow) {
+    int licznik = 0;
+    for(int i = 0; i < liczbaRzutow; i++) {
+        if(rzuty[i] == 6) {
+            licznik++;
+        }
+    }
+    cout << "Liczba szostek: " << licznik << endl;
+}
+
+void najwiekszyRzut(int rzuty[], int liczbaRzutow) {
+    if(liczbaRzutow == 0) {
+        cout << "Nie wykonano jeszcze zadnego rzutu." << endl;
+        return;
+    }
+    int maxRzut = rzuty[0];
+    for(int i = 1; i < liczbaRzutow; i++) {
+        if(rzuty[i] > maxRzut) {
+            maxRzut = rzuty[i];
+        }
+    }
+    cout << "Najwiekszy rzut: " << maxRzut << endl;
+}
+
 int main(int argc, char** argv) {
-	
-	int a, nr_rzutu = 0;
-	int rzuty[50];
-	
-	while(true){
-		cout<<"1 - wykonaj rzut kostka"<<endl;
-		cout<<"2 - wyswietl wszystkie rzuty"<<endl;
-		cout<<"3 - oblicz sume oczek"<<endl;
-		cout<<"4 - policz ile razy wypadla liczba 6"<<endl;
-		cout<<"5 - znajdz najwiekszy rzut"<<endl;
-		cout<<"6 - zakoncz program"<<endl;
-	
-	cin>>a;
-	
-if(a == 1){
-	nr_rzutu = dodajRzut(rzuty, nr_rzutu);
-} else if(a == 2){
-	wyswietlRzuty(rzuty, nr_rzutu);
-} else if(a == 3){
-	sumaOczek(rzuty, nr_rzutu);
-} else if(a == 4){
-	dodajRzut(rzuty, nr_rzutu);
-} else if(a == 5){
-	dodajRzut(rzuty, nr_rzutu);
-} else if(a == 6){
-	return 0;
-}
-	
-}
+    srand(time(0)); // Inicjalizacja generatora liczb losowych
+    
+    int a, nr_rzutu = 0;
+    int rzuty[50];
+    
+    while(true) {
+        cout << "\n1 - wykonaj rzut kostka" << endl;
+        cout << "2 - wyswietl wszystkie rzuty" << endl;
+        cout << "3 - oblicz sume oczek" << endl;
+        cout << "4 - policz ile razy wypadla liczba 6" << endl;
+        cout << "5 - znajdz najwiekszy rzut" << endl;
+        cout << "6 - zakoncz program" << endl;
+        cout << "Wybor: ";
+        
+        cin >> a;
+        
+        if(a == 1) {
+            nr_rzutu = dodajRzut(rzuty, nr_rzutu);
+        } else if(a == 2) {
+            wyswietlRzuty(rzuty, nr_rzutu);
+        } else if(a == 3) {
+            sumaOczek(rzuty, nr_rzutu);
+        } else if(a == 4) {
+            policzSzostki(rzuty, nr_rzutu); // Naprawione
+        } else if(a == 5) {
+            najwiekszyRzut(rzuty, nr_rzutu); // Naprawione
+        } else if(a == 6) {
+            break; // Uzywamy break zamiast return dla lepszej struktury
+        } else {
+            cout << "Niepoprawny wybor!" << endl;
+        }
+    }
 
-
-	return 0;
+    return 0;
 }
